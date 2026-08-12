3188e0581b870dba10b157e2b64b3d28da4fc25d
IsiVoltPro Mantenimiento <isiciozafracantos@gmail.com>
2026-08-12 20:22:34 +0200

    feat(v3): add React application entrypoint

---
+++ v3/frontend/src/main.tsx
@@ -0,0 +1,19 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router'
+import App from './app/App'
+import './styles/global.css'
+
+const root = document.getElementById('root')
+
+if (!root) {
+  throw new Error('No se ha encontrado el elemento #root')
+}
+
+createRoot(root).render(
+  <StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StrictMode>,
+)
