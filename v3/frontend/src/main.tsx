6af0d3927924d231700fb8f9f1579b60e6f8a7ae
IsiVoltPro Mantenimiento <isiciozafracantos@gmail.com>
2026-08-12 20:53:33 +0200

    feat(v3): load student admin styles

--- v3/frontend/src/main.tsx
+++ v3/frontend/src/main.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from 'react-router'
 import App from './app/App'
 import './styles/global.css'
 import './styles/admin-cms.css'
+import './styles/admin-students.css'
 
 const root = document.getElementById('root')
 
