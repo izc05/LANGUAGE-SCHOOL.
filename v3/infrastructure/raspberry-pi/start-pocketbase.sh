6453209cbe6e876e49ec3704cd13ae1ea29f8531
IsiVoltPro Mantenimiento <isiciozafracantos@gmail.com>
2026-08-17 23:57:39 +0200

    security: load PocketBase hooks in production

--- v3/infrastructure/raspberry-pi/start-pocketbase.sh
+++ v3/infrastructure/raspberry-pi/start-pocketbase.sh
@@ -5,9 +5,15 @@ PB_URL="${PB_URL:-}"
 PB_BIN="${PB_BIN:-/opt/language-school/pocketbase/pocketbase}"
 PB_DATA="${PB_DATA:-/var/lib/language-school/pb_data}"
 PB_MIGRATIONS="${PB_MIGRATIONS:-/opt/language-school/pocketbase/pb_migrations}"
+PB_HOOKS="${PB_HOOKS:-/opt/language-school/pocketbase/pb_hooks}"
 
 if [[ ! "$PB_URL" =~ ^http://127\.0\.0\.1:([0-9]{1,5})$ ]]; then
   echo 'PB_URL must use http://127.0.0.1:<port> with no path.' >&2
+  exit 1
+fi
+
+if [[ -z "${TURNSTILE_SECRET_KEY:-}" ]]; then
+  echo 'TURNSTILE_SECRET_KEY is required for the protected contact endpoint.' >&2
   exit 1
 fi
 
@@ -20,4 +26,5 @@ fi
 exec "$PB_BIN" serve \
   --http="127.0.0.1:$PB_PORT" \
   --dir="$PB_DATA" \
-  --migrationsDir="$PB_MIGRATIONS"
+  --migrationsDir="$PB_MIGRATIONS" \
+  --hooksDir="$PB_HOOKS"
